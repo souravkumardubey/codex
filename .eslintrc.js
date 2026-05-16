a10318e009e9a388e82a2a05044710ed549f769a
souravkumardubey <skdlp2000@gmail.com>
2026-05-17 02:54:15 +0530

    fix: configure ESLint and fix CI pipeline
    - Add ESLint with TypeScript support to root workspace
    - Add root .eslintrc.js configuration
    - Fix test scripts to use --passWithNoTests for CI

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+  },
+  ignorePatterns: ['dist', 'node_modules', '.next', 'coverage'],
+};
